15b62bb30289a9fd90a296c7913967219e491ba7
Costa <ancst@yandex.ru>
2021-11-17 14:11:30 +0300

    Skeleton

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const sessionConfig = {
+  name: 'super_hero',
+  secret: 'hello',
+  resave: false,
+  saveUnitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 14,
+    httpOnly: true,
+  },
+};
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(morgan('dev'));
+app.use(expressSession(sessionConfig));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Hello server', PORT);
+});
