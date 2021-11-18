2f03d6bec1e56c5464c12cf7e70fa51134da77d4
layabout <minyaeavmail@gmail.com>
2021-11-18 17:12:06 +0300

    registration done

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+// const regButton = document.querySelector('#regBtn');
+// regButton.addEventListener('click', async () => {
+//   const response = await fetch('/registration');
+// });
+
+// const logButton = document.querySelector('#logBtn');
+// logButton.addEventListener('click', async () => {
+//   logButton.render('/login');
+// });
