2f8d298523732eff44537bafd4d8fdefdc9ab422
YanaBagrova <yanabag@mail.ru>
2021-11-19 16:19:00 +0300

    Final

--- routes/logout.js
+++ routes/logout.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/logout', (req, res) => {
+router.post('/', (req, res) => {
   req.session.destroy((error) => {
     if (error) {
       return res.status(500).json({ message: 'Ошибка при удалении сессии' });
