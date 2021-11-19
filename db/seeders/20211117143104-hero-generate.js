ba2ca0a3760eaeeb02c89def14d53316732bb38b
layabout <minyaeavmail@gmail.com>
2021-11-19 10:17:23 +0300

    seed heroes

--- db/seeders/20211117143104-hero-generate.js
+++ db/seeders/20211117143104-hero-generate.js
@@ -11,15 +11,65 @@ module.exports = {
     */
     await queryInterface.bulkInsert('Heros', [
       {
-        name: 'Harley Quinn',
-        superpower: 'database',
+        name: 'Зашкварли Quinn',
+        superpower: 'Не воровка ...',
+        UserId: '1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'FatMan',
+        superpower: 'Сахарный диабет',
+        UserId: '1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Вжух',
+        superpower: 'Быстрее всех бежит за пивком',
+        UserId: '1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'MrЧесотка',
+        superpower: 'Оксана (ногти) +7964******',
+        UserId: '1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Челопук',
+        superpower: 'Видит в темноте,но только еду',
+        UserId: '1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Скочмэн',
+        superpower: 'Андэfinded',
+        UserId: '1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'СеньорСутенер ',
+        superpower: 'Брат Хью Хефнера',
+        UserId: '1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Вандермян',
+        superpower: 'Красиво делает',
+        UserId: '1',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
     ], {});
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     /**
      * Add commands to revert seed here.
      *
